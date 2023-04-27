6c9f8530ce5633254b45377f88cf5894bb846e93
Valeriy <seveneight78@list.ru>
2023-04-27 16:46:32 +0300

    chtoto

--- frontend/src/features/todos/TodoItem.tsx
+++ frontend/src/features/todos/TodoItem.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Todo } from './types/Todo';
+import { useDispatch } from 'react-redux';
+import { Todo, TodoId } from './types/Todo';
 
 function TodoItem({ todo }: { todo: Todo }): JSX.Element {
+  const dispatch = useDispatch();
+
+  const onHandleRemove = async (todoId: number): Promise<void> => {
+    const response = await fetch(`/api/todos/${todoId}`, { method: 'delete' });
+    const data = await response.json();
+    dispatch({ type: 'REMOVE_TODO', payload: Number(data) });
+  };
+
   return (
     <div className="div_todo">
       <h2>{todo.title}</h2>
       <p>{todo.description}</p>
-      <button type="button">Удалить</button>
+      <button type="button" onClick={() => onHandleRemove(todo.id)}>Удалить</button>
       <button type="button"><img src="https://st18.stpulscen.ru/images/localized/002/772/784_original.jpg" style={{ width: '20px', height: '20px' }} alt="galka" /></button>
     </div>
   );
--- frontend/src/features/todos/TodosList.tsx
+++ frontend/src/features/todos/TodosList.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import TodoItem from './TodoItem';
+import AddTodoForm from './AddTodoForm';
 
 function TodosList(): JSX.Element {
   const { todos } = useSelector((store: RootState) => store.todos);
 
   return (
     <div>
+      <AddTodoForm />
       {todos.map((todo) => (<TodoItem key={todo.id} todo={todo} />))}
     </div>
   );
