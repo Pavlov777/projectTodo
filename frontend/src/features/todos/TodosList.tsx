45a21d4fb80986125b6e918ded6002105602bc20
Valeriy <seveneight78@list.ru>
2023-04-27 17:15:29 +0300

    tryIt : fix.

--- frontend/src/features/todos/TodosList.tsx
+++ frontend/src/features/todos/TodosList.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import TodoItem from './TodoItem';
-import AddTodoForm from './AddTodoForm';
 
 function TodosList(): JSX.Element {
   const { todos } = useSelector((store: RootState) => store.todos);
 
   return (
     <div>
-      <AddTodoForm />
       {todos.map((todo) => (<TodoItem key={todo.id} todo={todo} />))}
     </div>
   );
